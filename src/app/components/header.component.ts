68a289119c2b7d3d142a24efc56422aa62201715
Andrés <andresin@gmail.com>
2019-04-25 14:41:08 -0300

    Se extiende HEADER

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AgendasService } from '../services/agendas.services';
 import { Plex } from '@andes/plex';
@@ -7,18 +7,22 @@ import { PacienteService } from '../services/paciente.service';
 import { Location } from '@angular/common';
 
 
-
 @Component({
     selector: 'header-component',
     templateUrl: 'header.html',
-    styleUrls: ['header.css']
+    styleUrls: ['header.scss']
 })
 export class HeaderComponent implements OnInit {
-
-    private prestacionSeleccionada;
-    private agendas = [];
-    private listadoTurnos = [];
-    private paciente;
+    @Input() heading: any = {
+        principal: '',
+        secundario: '',
+    };
+    public prestacionSeleccionada;
+    public agendas = [];
+    public listadoTurnos = [];
+    public paciente;
+    public fecha = moment().format('LLL') + ' hs';
+    documento: any;
     constructor(
         private agendasService: AgendasService,
         private turnosService: TurnosService,
@@ -31,7 +35,7 @@ export class HeaderComponent implements OnInit {
 
     ngOnInit() {
         this.paciente = this.pacienteService.getPacienteValor();
-
+        this.documento = this.paciente.documento.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');;
     }
 
 
