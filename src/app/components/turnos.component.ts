fdb00cd86ca428e61de9b04a1f5514d2b8516819
Juliana <juli.otero01@gmail.com>
2019-11-11 15:57:20 -0300

    refactor(totem): Evitar que se pueda sacar más de un turno para el mismo paciente y prestación. Mostrar datos del primer turno asignado.

--- src/app/components/turnos.component.ts
+++ src/app/components/turnos.component.ts
@@ -5,120 +5,130 @@ import { Plex } from '@andes/plex';
 import { TurnosService } from '../services/turnos.service';
 import { PacienteService } from '../services/paciente.service';
 @Component({
-    templateUrl: 'turnos.html',
-    styleUrls: ['turno.scss']
+  templateUrl: 'turnos.html',
+  styleUrls: ['turno.scss']
 })
 export class TurnosComponent implements OnInit {
 
-    public heading = {
-        principal: `Seleccione un día y horario`,
-        secundario: `Horarios disponibles para la `
+  public heading = {
+    principal: `Seleccione un día y horario`,
+    secundario: `Horarios disponibles para la `
+  };
+  private paciente;
+  public prestacionSeleccionada;
+  public agendas = [];
+  public tieneTurnos;
+  public turnoAsignado;
+  public listadoTurnos = [];
+  public confirmarTurno = false;
+  public turnoSeleccionado;
+  public disabled = false;
+  public diaString = '';
+  private dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+  scrolled = false;
+  @ViewChild('inicioTurnos')
+  inicioTurnosRef: ElementRef;
+  @ViewChild('finTurnos')
+  finTurnosRef: ElementRef;
+
+  constructor(
+    private agendasService: AgendasService,
+    private turnosService: TurnosService,
+    private route: ActivatedRoute,
+    private plex: Plex,
+    private pacienteService: PacienteService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.paciente = this.pacienteService.getPacienteValor();
+    if (!this.paciente) {
+      this.router.navigate(['buscar']);
+    }
+    this.route.queryParams.subscribe(params => {
+      this.prestacionSeleccionada = params;
+      if (this.prestacionSeleccionada && this.prestacionSeleccionada.conceptId) {
+        this.heading.secundario += ` <b>${this.prestacionSeleccionada.term}</b>`;
+        this.turnosService.getProximos({ turnosProximos: true, pacienteId: this.paciente.id, conceptId: this.prestacionSeleccionada.conceptId, organizacion: true }).subscribe(turnos => {
+          this.turnoAsignado = turnos[0];
+          this.tieneTurnos = (turnos[0].length > 0);
+          console.log(this.turnoAsignado);
+          if (!this.tieneTurnos) {
+            this.agendasService.getAgendas({ prestacion: this.prestacionSeleccionada.conceptId }).subscribe((agendas) => {
+              this.agendas = agendas;
+              this.listadoTurnos = this.parsearTurnos(agendas);
+            }, (error) => {
+              this.plex.info('danger', error, 'Error');
+            });
+          }
+        });
+
+      }
+    });
+  }
+
+  parsearTurnos(agendas) {
+    let agendasParseadas = agendas.map(agenda => {
+      let turnos = agenda.bloques.turnos.filter(turno => turno.estado === 'disponible' && turno.horaInicio >= new Date());
+      return {
+        idAgenda: agenda._id,
+        idBloque: agenda.bloques._id,
+        turno: turnos[0],
+        profesional: agenda.profesionales && agenda.profesionales.length > 0 ? agenda.profesionales[0] : null,
+        prestacion: this.prestacionSeleccionada
+      };
+    });
+    return agendasParseadas;
+  }
+
+  selectTurno(turno) {
+    let numDia = turno.turno.horaInicio.getDay();
+    let diferenciaDias = moment(turno.turno.horaInicio).diff(moment().startOf('day'), 'days');
+    this.diaString = (diferenciaDias === 0) ? 'Hoy' : (diferenciaDias === 1) ? 'Mañana' : this.dias[numDia];
+    this.turnoSeleccionado = turno;
+
+    this.heading = {
+      principal: 'Confirmar Turno',
+      secundario: '¡No olvides anotar los datos de tu Turno!'
     };
-    private paciente;
-    public prestacionSeleccionada;
-    public agendas = [];
-    public listadoTurnos = [];
-    public confirmarTurno = false;
-    public turnoSeleccionado;
-    public disabled = false;
-    public diaString = '';
-    private dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
-    scrolled = false;
-    @ViewChild('inicioTurnos')
-    inicioTurnosRef: ElementRef;
-    @ViewChild('finTurnos')
-    finTurnosRef: ElementRef;
-
-    constructor(
-        private agendasService: AgendasService,
-        private turnosService: TurnosService,
-        private route: ActivatedRoute,
-        private plex: Plex,
-        private pacienteService: PacienteService,
-        private router: Router,
-    ) { }
-
-    ngOnInit() {
-        this.paciente = this.pacienteService.getPacienteValor();
-        if (!this.paciente) {
-            this.router.navigate(['buscar']);
-        }
-        this.route.queryParams.subscribe(params => {
-            this.prestacionSeleccionada = params;
-            if (this.prestacionSeleccionada && this.prestacionSeleccionada.conceptId) {
-                this.heading.secundario += ` <b>${this.prestacionSeleccionada.term}</b>`;
-                this.agendasService.getAgendas({ prestacion: this.prestacionSeleccionada.conceptId }).subscribe((agendas) => {
-                    this.agendas = agendas;
-                    this.listadoTurnos = this.parsearTurnos(agendas);
-                }, (error) => {
-                    this.plex.info('danger', error, 'Error');
-                });
-            }
-        });
+    if (!this.paciente) {
+      this.plex.info('danger', 'Paciente no encontrado', 'Error');
     }
 
-    parsearTurnos(agendas) {
-        let agendasParseadas = agendas.map(agenda => {
-            let turnos = agenda.bloques.turnos.filter(turno => turno.estado === 'disponible' && turno.horaInicio >= new Date());
-            return {
-                idAgenda: agenda._id,
-                idBloque: agenda.bloques._id,
-                turno: turnos[0],
-                profesional: agenda.profesionales && agenda.profesionales.length > 0 ? agenda.profesionales[0] : null,
-                prestacion: this.prestacionSeleccionada
-            };
-        });
-        return agendasParseadas;
-    }
+  }
 
-    selectTurno(turno) {
-        let numDia = turno.turno.horaInicio.getDay();
-        let diferenciaDias = moment(turno.turno.horaInicio).diff(moment().startOf('day'), 'days');
-        this.diaString = (diferenciaDias === 0) ? 'Hoy' : (diferenciaDias === 1) ? 'Mañana' : this.dias[numDia];
-        this.turnoSeleccionado = turno;
+  horaInicio(horaInicio) {
+    return moment(horaInicio).calendar();
+  }
 
-        this.heading = {
-            principal: 'Confirmar Turno',
-            secundario: '¡No olvides anotar los datos de tu Turno!'
-        }
+  guardar() {
+    this.disabled = true;
+    this.turnosService.save(this.turnoSeleccionado, this.paciente, { showError: false }).subscribe((resultado) => {
+      this.router.navigate(['/publicidad'], { queryParams: { textoTurno: true } });
+    }, (error) => {
+      this.router.navigate(['buscar']);
+    });
+  }
 
-        if (!this.paciente) {
-            this.plex.info('danger', 'Paciente no encontrado', 'Error');
-        }
+  salir() {
+    this.disabled = true;
+    this.router.navigate(['buscar']);
+  }
 
-    }
+  corregir() {
+    this.turnoSeleccionado = null;
+  }
 
-    horaInicio(horaInicio) {
-        return moment(horaInicio).calendar();
-    }
-
-    guardar() {
-        this.disabled = true;
-        this.turnosService.save(this.turnoSeleccionado, this.paciente, { showError: false }).subscribe((resultado) => {
-            this.router.navigate(['/publicidad'], { queryParams: { textoTurno: true } });
-        }, (error) => {
-            this.router.navigate(['buscar']);
-        });
-    }
-
-    salir() {
-        this.disabled = true;
-        this.router.navigate(['buscar']);
-    }
-
-    corregir() {
-        this.turnoSeleccionado = null;
-    }
-
-    scrollTop() {
-        this.inicioTurnosRef.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        this.scrolled = false;
-    }
-    scrollDown() {
-        this.finTurnosRef.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        this.scrolled = true;
-    }
+  scrollTop() {
+    this.inicioTurnosRef.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    this.scrolled = false;
+  }
+  scrollDown() {
+    this.finTurnosRef.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    this.scrolled = true;
+  }
 }
 
 
--- src/app/services/turnos.service.ts
+++ src/app/services/turnos.service.ts
@@ -1,33 +1,35 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Server } from '@andes/shared';
-import { ANDES_KEY } from '../../config.private';
 
 
 @Injectable()
 export class TurnosService {
-    // URL to web api
-    private turnosUrl = '/modules/turnos';
+  // URL to web api
+  private turnosUrl = '/modules/turnos';
 
-    constructor(private server: Server) { }
+  constructor(private server: Server) { }
 
-    save(turno: any, paciente: any, options: any = {}): Observable<any> {
-        // Datos del Turno
-        let datosTurno = {
-            idAgenda: turno.idAgenda,
-            idTurno: turno.turno._id,
-            idBloque: turno.idBloque,
-            paciente,
-            tipoPrestacion: turno.prestacion,
-            tipoTurno:  moment(turno.turno.horaInicio).format('DD/MM/YYYY') === moment(new Date()).format('DD/MM/YYYY') ? 'delDia' : 'programado',
-            emitidoPor: 'totem',
-            nota: 'Turno pedido desde totem',
-            motivoConsulta: ''
-        };
-        if (turno.idAgenda) {
-            return this.server.patch(this.turnosUrl + '/turno/' + turno.turno._id + '/bloque/' + turno.idBloque + '/agenda/' + turno.idAgenda, datosTurno, options);
-        }
+  save(turno: any, paciente: any, options: any = {}): Observable<any> {
+    // Datos del Turno
+    let datosTurno = {
+      idAgenda: turno.idAgenda,
+      idTurno: turno.turno._id,
+      idBloque: turno.idBloque,
+      paciente,
+      tipoPrestacion: turno.prestacion,
+      tipoTurno: moment(turno.turno.horaInicio).format('DD/MM/YYYY') === moment(new Date()).format('DD/MM/YYYY') ? 'delDia' : 'programado',
+      emitidoPor: 'totem',
+      nota: 'Turno pedido desde totem',
+      motivoConsulta: ''
+    };
+    if (turno.idAgenda) {
+      return this.server.patch(this.turnosUrl + '/turno/' + turno.turno._id + '/bloque/' + turno.idBloque + '/agenda/' + turno.idAgenda, datosTurno, options);
     }
+  }
 
+  getProximos(params: any): Observable<any[]> {
+    return this.server.get(this.turnosUrl + '/search', { params: params, showError: true });
+  }
 }
 
